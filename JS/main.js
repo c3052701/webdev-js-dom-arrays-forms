c530abd4415e5f0a5d6f05fc25351496b5f9ea35
Rimu <c3052701@hallam.shu.ac.uk>
2023-11-17 09:59:40 +0000

    changed background Color

--- JS/main.js
+++ JS/main.js
@@ -8,8 +8,39 @@
     // silly example:)
     // document.querySelector("nav ul li a").setAttribute("href","http://www.google.co.uk");
 
+    // Events
+    // document.getElementById("myTestEvent").addEventListener("click",function(){
+    // console.info("hi");
+        document.getElementById("myTestEvent").addEventListener("click",myFunction)
+        function myFunction(ev) 
+        {
+            console.info("hi");
+        }
 
-    // code ends here
+        document.querySelector(".red").addEventListener('click', function(ev){
+        document.querySelector('body').setAttribute('class', "redBack");
+        })
+
+        document.querySelector(".green").addEventListener('click', function(ev){
+        document.querySelector('body').setAttribute('class', "greenBack");
+        })
+    
+        document.querySelector(".blue").addEventListener('click', function(ev){
+        document.querySelector('body').setAttribute('class',"blueBack");
+        })
+        document.querySelector(".reset").addEventListener('click', function(ev){
+        document.querySelector('body').setAttribute('class',"whiteBack");
+        })
+        
+        
+        
+    
+    
+    
+    
+    
+    
+        // code ends here
 })();
 
 

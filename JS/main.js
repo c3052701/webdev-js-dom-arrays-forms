14f2933b3a115120d6a98db1958fb761a0edf827
Rimu <c3052701@hallam.shu.ac.uk>
2023-11-17 09:12:47 +0000

    IIFE

---
+++ JS/main.js
@@ -0,0 +1,17 @@
+// IIFE
+
+(function(){
+    // code starts here
+    
+    document.getElementById("myHeading").innerHTML="Suraiya";
+    document.querySelector("nav ul li").setAttribute("class","currentPage")
+    // silly example:)
+    // document.querySelector("nav ul li a").setAttribute("href","http://www.google.co.uk");
+
+
+    // code ends here
+})();
+
+
+
+

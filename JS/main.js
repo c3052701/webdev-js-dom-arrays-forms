d70db2a099e0058234a0ddbc13f6f83aa5c0ee0c
Rimu <c3052701@hallam.shu.ac.uk>
2023-11-17 10:55:13 +0000

    image rotator

--- JS/main.js
+++ JS/main.js
@@ -17,27 +17,51 @@
             console.info("hi");
         }
 
-        document.querySelector(".red").addEventListener('click', function(ev){
-        document.querySelector('body').setAttribute('class', "redBack");
-        })
+        // document.querySelector(".red").addEventListener('click', function(ev){
+        // document.querySelector('body').setAttribute('class', "redBack");
+        // })
 
-        document.querySelector(".green").addEventListener('click', function(ev){
-        document.querySelector('body').setAttribute('class', "greenBack");
-        })
+        // document.querySelector(".green").addEventListener('click', function(ev){
+        // document.querySelector('body').setAttribute('class', "greenBack");
+        // })
     
-        document.querySelector(".blue").addEventListener('click', function(ev){
-        document.querySelector('body').setAttribute('class',"blueBack");
-        })
-        document.querySelector(".reset").addEventListener('click', function(ev){
-        document.querySelector('body').removeAttribute('class');
-        })
+        // document.querySelector(".blue").addEventListener('click', function(ev){
+        // document.querySelector('body').setAttribute('class',"blueBack");
+        // })
+        // document.querySelector(".reset").addEventListener('click', function(ev){
+        // document.querySelector('body').removeAttribute('class');
+        // })
         
     
+    let colourButtons = document.querySelectorAll(".colPicker")
+    console.dir(colourButtons)
         
-        
-    
-    
-    
+    for(let i=0; i < colourButtons.length; i++){
+    colourButtons[i].addEventListener("click", chgColour)
+    }    
+    function chgColour(ev){
+    console.dir(ev.target.classList)
+    }
+    let colourPicked = ev.target.classList[0] + "Back";
+    bodyElement.setAttribute("class", colourPicked);
+
+// image rotator
+let imageAr = [
+    'images/view1.jpg', 
+    'images/view2.jpg', 
+    'images/view3.jpg', 
+    'images/view4.jpg', 
+    'images/view5.jpg', 
+    'images/view6.jpg'
+    ];
+
+    let imageCount = 0;
+    setInterval(function(){
+        document.getElementById("myImages")
+        .setAttribute("src",imageAr[imageCount]);
+        imageCount++;
+    }, 1000);
+
     
     
     
